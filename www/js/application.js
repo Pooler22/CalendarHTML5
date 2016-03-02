9865c423c85a296101a54d7ebad37f12455c6343
pooler22 <rudypawel12@gmail.com>
2016-03-02 16:46:59 +0100

    widok strony główna: przygotowanie

--- www/js/application.js
+++ www/js/application.js
@@ -1,21 +1,28 @@
 function Application(UIContext) {
-    this._uiContextClass = UIContext;
-    this._initialized = false;
+  this._uiContextClass = UIContext;
+  this._initialized = false;
 };
 Application.prototype.init = function() {
-    if (this._uiContextClass && !this._initialized) {
-        this._initialized = true;
-        var UI = new this._uiContextClass();
-        UI.init();
+  if (this._uiContextClass && !this._initialized) {
+    this._initialized = true;
+    var UI = new this._uiContextClass();
+    UI.init();
 
-        // hello-button event handler
-        UI.button('hello-button').click( function() {
-            var helloLabel = document.getElementById("hello-label");
-            helloLabel.innerHTML = "...World!";
-        });
-    }
+    var dialog = UI.dialog('dialog-period');
+    var show = UI.button('set-period').click(function() {
+      dialog.show();
+    });
+    var hide = UI.button('hide-dialog-period').click(function() {
+      dialog.hide();
+    });
+
+    // hello-button event handler
+    // UI.button('hello-button').click(function() {
+    //   var helloLabel = document.getElementById("hello-label");
+    //   helloLabel.innerHTML = "...World!";
+    // });
+  }
 };
 Application.prototype.initialized = function() {
-    return this._initialized;
+  return this._initialized;
 };
-
